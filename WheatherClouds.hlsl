a1330a4de9cfcb83f08b176462ca3fdb4c581c16
neiji93270 <nejdet.altinisik@yahoo.fr>
2025-09-12 21:13:52 +0200

    Create WheatherClouds.hlsl

---
+++ WheatherClouds.hlsl
@@ -0,0 +1,6 @@
+//a process in high energy weather cloud can be 
+//sampmed if the richardson bushlan equation is isomorph for a close timelapse
+//to another sampled textures. to be isomorph, we d better investigate 
+//rapidely pc ram for large calculuses. to avoid gpu overhead,
+//we have to focus on heat transfer mostly and not beam physics directly.
+//as
