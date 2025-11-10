3972839f7fc9180ce4908cc7827cf3affb10e6e4
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-10 11:36:27 +0100

    Create Dontparameterizeallonebeamscattering.h

---
+++ Dontparameterizeallonebeamscattering.h
@@ -0,0 +1,4 @@
+//we can use log log log plot on feynman motivee.
+//but as it is z rough idea, we better have to sample 960 gpus (model like oarc model).
+//plus, to enhance the computation bit still roughly,
+//we can omit parametirizing the output model parameters.
